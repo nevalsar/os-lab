ea3ba4a3e20438870a3e8179f5618168d64cf1a8
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2015-04-17 22:12:43 +0530

    Add mutual exclusion for the global variables
    Signed-off-by: Nevin Valsaraj <nevin.valsaraj32@gmail.com>

--- assign7/crawler.cpp
+++ assign7/crawler.cpp
@@ -44,13 +44,26 @@ pthread_mutex_t done_mutex;
 queue<URL> to_do_next;
 pthread_mutex_t to_do_next_mutex;
 
+int waiting_threads;
+pthread_mutex_t waiting_threads_mutex;
+
+pthread_cond_t condition;
+pthread_mutex_t condition_mutex;
+
 int main(void){
 
 	URL master;
+	pthread_mutex_init(&level_mutex, NULL);
+	pthread_mutex_init(&to_do_mutex, NULL);
+	pthread_mutex_init(&done_mutex, NULL);
+	pthread_mutex_init(&to_do_next_mutex, NULL);
+	pthread_mutex_init(&waiting_threads_mutex, NULL);
 
 	fscanf(stdin,"%10d",&final_level);
 
 	level = 1;
+	waiting_threads = 0;
+	pthread_cond_init (&condition, NULL);
 
 	master.urlname = MASTER_URL;
 	master.thread_id = pthread_self();
@@ -60,8 +73,15 @@ int main(void){
 
 	spawn_threads();
 	print_result();
-	pthread_exit(NULL);
 
+	pthread_cond_destroy(&condition);
+	pthread_mutex_destroy(&level_mutex);
+	pthread_mutex_destroy(&to_do_mutex);
+	pthread_mutex_destroy(&done_mutex);
+	pthread_mutex_destroy(&to_do_next_mutex);
+	pthread_mutex_destroy(&waiting_threads_mutex);
+
+	pthread_exit(NULL);
 	return 0;
 }
 
@@ -85,35 +105,77 @@ void* crawl(void*){
 	while(1){
 		URL current_URL;
 
-		if(to_do.empty())
+		pthread_mutex_lock(&to_do_mutex);
+		if(to_do.empty()) {
+			cout <<"Thread " <<pthread_self() << " found TODO queue empty\n";
+			pthread_mutex_unlock(&to_do_mutex);
 			level_update();
+		} else {
+			current_URL = to_do.front();
+			to_do.pop();
+			cout <<"Thread " <<pthread_self() << " : " <<current_URL.urlname <<"\n";
+			pthread_mutex_unlock(&to_do_mutex);
 
-		current_URL = to_do.front();
-		to_do.pop();
+			pthread_mutex_lock(&done_mutex);
+			if(url_list.find(current_URL.urlname) != url_list.end()) {
+				pthread_mutex_unlock(&done_mutex);
+				continue;
+			}
+			else
+				pthread_mutex_unlock(&done_mutex);
 
-		if(url_list.find(current_URL.urlname) != url_list.end())
-			continue;
+			extract_URLs(current_URL,to_do_next);
 
-		extract_URLs(current_URL,to_do_next);
-
-		url_list.insert(current_URL.urlname);
-		done_list.push(current_URL);
+			pthread_mutex_lock(&done_mutex);
+			{
+				url_list.insert(current_URL.urlname);
+				done_list.push(current_URL);
+			}
+			pthread_mutex_unlock(&done_mutex);
+		}
 	}
 	return NULL;
 }
 
 int level_update(){
 
-	if(level < final_level){
+	pthread_mutex_lock(&waiting_threads_mutex);
+	waiting_threads++;
+
+	if (waiting_threads < 5) {
+		pthread_mutex_unlock(&waiting_threads_mutex);
+		pthread_cond_wait (&condition, &condition_mutex);
+		cout <<"Thread " <<pthread_self() <<" woke up\n";
+	} else {
+		waiting_threads = 0;
+		pthread_mutex_unlock(&waiting_threads_mutex);
 		level++;
 		while(!to_do_next.empty()){
 			to_do.push(to_do_next.front());
 			to_do_next.pop();
 		}
+		pthread_cond_broadcast(&condition);
 	}
-	else{
+	pthread_mutex_lock(&level_mutex);
+	if (level > final_level) {
+		pthread_mutex_unlock(&level_mutex);
+		cout <<"Thread " <<pthread_self() << " exiting\n";
 		pthread_exit(NULL);
 	}
+	else
+		pthread_mutex_unlock(&level_mutex);
+
+	// if(level < final_level){
+	// 	level++;
+	// 	while(!to_do_next.empty()){
+	// 		to_do.push(to_do_next.front());
+	// 		to_do_next.pop();
+	// 	}
+	// }
+	// else{
+	// 	pthread_exit(NULL);
+	// }
+
 	return 0;
 }
 
@@ -155,7 +217,11 @@ int extract_URLs(URL& url,queue<URL>& queue){
 							newURL.urlname = iter->second;
 							newURL.thread_id = pthread_self();
 							newURL.depth = level;
-							queue.push(newURL);
+							pthread_mutex_lock(&to_do_next_mutex);
+							{
+								queue.push(newURL);
+							}
+							pthread_mutex_unlock(&to_do_next_mutex);
 						}
 					}
 				}
